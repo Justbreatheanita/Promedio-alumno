8aae4c655967a35c54a1c04e7f255fcf8e467e64
justbreatheanita <justbreatheanita@gmail.com>
2025-06-11 18:50:52 -0400

    Promedio de notas de Anita

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,20 @@
+//Sumar notas
+
 function Sumadenotas (n1,n2,n3,n4,n5,n6) {
     return n1 + n2 + n3 + n4 + n5 + n6;
     }
 
 const Alumno = [6, 8, 9, 2, 5, 10];
-const resultado = Sumadenotas(...Alumno); //los ... permite que los números funciones como valores separados
+const resultado = Sumadenotas(...Alumno); // los (...) permite que los números funciones como valores separados o sino no me funciona
 
 console.log ("La suma de notas de Anita es", resultado);
+
+//Sacar promedio
+
+function Promedio (resultado, cantidad) {
+    return resultado / cantidad;
+    }
+
+const Anita = Promedio(resultado, Alumno.length); // Alumno.length dice cuantas notas hay en Alumno
+
+console.log ("El promedio de notas de Anita es", Anita);
